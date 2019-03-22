6ee0c9de0be5960e9e5342a16ad392027454bfcc
sean <sean@seans-labtop>
2019-03-22 15:55:13 -0400

    Java Project Initial Commit

---
+++ Overseer/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author sean
+ *
+ */
+module overseer
+{
+}
---
+++ Overseer/src/server/SendMessage.java
@@ -0,0 +1,11 @@
+package server;
+
+public class SendMessage
+{
+  private byte type;
+  //0 = CONNECTION ACCEPT
+  //1 = CONNECTION DENY
+  //2 = 
+  
+  private String[] genomes; //TODO Integrate with Genome class
+}
